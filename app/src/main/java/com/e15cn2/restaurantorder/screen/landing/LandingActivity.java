76a7de510b134f15a17a85cceddb7b9848770e63
Canh Tran <tran.van.canh@framgia.com>
2019-04-23 06:59:15 +0700

    init project

---
+++ app/src/main/java/com/e15cn2/restaurantorder/screen/landing/LandingActivity.java
@@ -0,0 +1,15 @@
+package com.e15cn2.restaurantorder.screen.landing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.e15cn2.restaurantorder.R;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_sign_up);
+    }
+}
